219c362baf7defecddf8df1621a8de6840653e85
RashikAnsar <sk.ansar46@gmail.com>
2019-08-04 13:33:38 +0530

    C:/Program Files/Git/shop showing collection-item

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import './collection-preview.styles.scss';
 
+import CollectionItem from '../collection-item/collection-item.component';
+
 const CollectionPreview = ({title, items}) => (
     <div className="collection-preview">
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview">
             {
-                items.filter((items,index) => index < 4).map((item) => <div key={item.id}>{item.name}</div>)
+                items
+                    .filter((item,index) => index < 4)
+                    .map(({id, ...otherItemProps}) => <CollectionItem key={id} {...otherItemProps} />)
             }
         </div>
     </div>
