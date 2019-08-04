6ee7457b24aaa098f3a856ca14c809d2f9812b94
RashikAnsar <sk.ansar46@gmail.com>
2019-08-04 13:19:50 +0530

    C:/Program Files/Git/shop page using collection preview

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => <h1>Hello from HatsPage</h1>
 
 function App() {
   return <div>
     <Switch>
       <Route exact path='/' component={HomePage} />
-      <Route path='/shop/hats' component={HatsPage} />
+      <Route path='/shop' component={ShopPage} />
     </Switch>
   </div>;
 }
