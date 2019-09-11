d58eef5fce3722d122d3fbaafbc4628882efad65
RashikAnsar <sk.ansar46@gmail.com>
2019-09-11 15:18:04 +0530

    styling custom button

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -52,9 +52,14 @@ class SignIn extends React.Component {
                         onChange={this.handleChange}
                         required
                     />
-                    
-                    <CustomButton type="submit">Sign In</CustomButton>
-                    <CustomButton onClick={signInWithGoogle}>Sign in with Google</CustomButton>
+
+                    <div className="buttons">
+                        <CustomButton type="submit">Sign In</CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                            Sign in with Google
+                        </CustomButton>
+                    </div>
+
                 </form>
             </div>
         );
