a9227895d296d8ed70e172fa1fb621895c2b1e2b
RashikAnsar <sk.ansar46@gmail.com>
2019-09-11 15:07:41 +0530

    show signin status in header and signout

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { auth } from '../../firebase/firebase.utils'
+
 import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
- 
-const Header = () => (
+
+const Header = ({ currentUser }) => (
     <div className="header">
         <Link className="logo-container" to="/">
             <Logo className="logo" />
@@ -12,6 +14,16 @@ const Header = () => (
         <div className="options">
             <Link className="option" to="/shop">SHOP</Link>
             <Link className="option" to="/shop">CONTACT</Link>
+            {
+                currentUser ?
+                    <div className="option" onClick={() => auth.signOut()}>
+                        SIGN OUT
+                    </div>
+                    :
+                    <Link className="option" to='/signin'>
+                        SIGN IN
+                    </Link>
+            }
         </div>
     </div>
 );
