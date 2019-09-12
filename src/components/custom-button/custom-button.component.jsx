bc77d049656b3d487e560a18c69a44a74898e492
RashikAnsar <sk.ansar46@gmail.com>
2019-09-12 19:24:58 +0530

    add to cart button

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButtom = ({ children, isGoogleSignIn, ...otherProps }) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
+const CustomButtom = ({ children, inverted ,isGoogleSignIn, ...otherProps }) => (
+    <button className={`${inverted ? 'inverted': ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
         {children}
     </button>
 );
