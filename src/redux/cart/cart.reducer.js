fd642c081acb310aedf7b661340dcde404909949
RashikAnsar <sk.ansar46@gmail.com>
2019-09-13 22:05:07 +0530

    add multiple items to cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,4 +1,5 @@
 import { CartActionTypes } from './cart.types';
+import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -8,9 +9,9 @@ const INITIAL_STATE = {
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case CartActionTypes.TOGGLE_CART_HIDDEN:
-            return { ...state, hidden: !state.hidden }
+            return { ...state, hidden: !state.hidden };
         case CartActionTypes.ADD_ITEM:
-            return { ...state, cartItems: [...state.cartItems, action.payload] }
+            return addItemToCart(state.cartItems, action.payload)
         default:
             return state;
     }
