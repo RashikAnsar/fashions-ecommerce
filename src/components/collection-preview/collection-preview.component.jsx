a0389e5ffb12605d44e432b32632f7b32614e520
RashikAnsar <sk.ansar46@gmail.com>
2019-09-13 20:56:39 +0530

    cart item reducer

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -4,14 +4,13 @@ import './collection-preview.styles.scss';
 
 import CollectionItem from '../collection-item/collection-item.component';
 
-const CollectionPreview = ({title, items}) => (
+const CollectionPreview = ({ title, items }) => (
     <div className="collection-preview">
         <h1 className="title">{title.toUpperCase()}</h1>
         <div className="preview">
-            {
-                items
-                    .filter((item,index) => index < 4)
-                    .map(({id, ...otherItemProps}) => <CollectionItem key={id} {...otherItemProps} />)
+            {items
+                .filter((item, index) => index < 4)
+                .map((item) => <CollectionItem key={item.id} item={item} />)
             }
         </div>
     </div>
