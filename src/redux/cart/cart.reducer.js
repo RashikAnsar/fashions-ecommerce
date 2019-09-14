ddb3ba11ce557aa4c0b44f877ac29afb0522b44e
RashikAnsar <sk.ansar46@gmail.com>
2019-09-14 22:25:36 +0530

    increase or decrease the quantity of an entity at checkout

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import { CartActionTypes } from './cart.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
     hidden: true,
@@ -12,6 +12,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return { ...state, hidden: !state.hidden };
         case CartActionTypes.ADD_ITEM:
             return { ...state, cartItems: addItemToCart(state.cartItems, action.payload) };
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            }
         case CartActionTypes.CLEAR_ITEM_FROM_CART:
             return {
                 ...state,
