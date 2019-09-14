cf543206e8e1a0ae620592238287384270f9a590
RashikAnsar <sk.ansar46@gmail.com>
2019-09-14 22:00:09 +0530

    remove item from cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -12,6 +12,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return { ...state, hidden: !state.hidden };
         case CartActionTypes.ADD_ITEM:
             return { ...state, cartItems: addItemToCart(state.cartItems, action.payload) };
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
+            }
         default:
             return state;
     }
