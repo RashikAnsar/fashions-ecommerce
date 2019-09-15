dbce0a9080062c9adf85e910a28cd529129c72c9
RashikAnsar <sk.ansar46@gmail.com>
2019-09-15 16:39:53 +0530

    style update

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -7,7 +7,7 @@ import CollectionItem from '../collection-item/collection-item.component';
 const CollectionPreview = ({ title, items }) => (
     <div className="collection-preview">
         <h1 className="title">{title.toUpperCase()}</h1>
-        <div className="preview">
+        <div className="items">
             {items
                 .filter((item, index) => index < 4)
                 .map((item) => <CollectionItem key={item.id} item={item} />)
